22a563110b415bbf122135f242bcbd1a65e28d86
peneir20@gmail.com <arthur.p31@aluno.ifsc.edu.br>
2025-11-07 11:11:40 -0300

    Corrigidas texturas, feito código mobile

--- Assets/bola.cs
+++ Assets/bola.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class bola : MonoBehaviour
 {
 
-    public float x, z;
+    private float x, z;
+    public float maxX, maxZ;
+    private Vector2 pi;
+    private Vector2 pf;
     float velocidade;
     LineRenderer lr;
     Rigidbody rb;
@@ -19,22 +21,12 @@ public class bola : MonoBehaviour
         {
             Debug.Log("Adicionar LineRenderer");
         }
+        lr.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        lr.SetPosition(0, transform.position);
-        lr.SetPosition(1, new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z));
-        if(Input.GetButtonDown("Jump") && lr.enabled)
-        {
-            rb.AddForce(new Vector3(2 * x, 0, 2 * z), ForceMode.Impulse);
-                lr.enabled = false;
-            /* if(GameManager.gm )
-            {
-                GameManager.gm.tacada();
-            }*/
-        }
         velocidade = rb.velocity.magnitude;
         if(velocidade < 0.05f)
         {
@@ -44,6 +36,53 @@ public class bola : MonoBehaviour
         } else
         {
             lr.enabled = false;
+        }
+
+        if(lr.enabled)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                Touch t =Input.GetTouch(i);
+                if(t.phase == TouchPhase.Began)
+                {
+                    pi = t.position;
+                    pf = t.position;
+                    x = 0;
+                    z = 0;
+                    lr.enabled = true;
+                    lr.SetPosition(0, transform.position);
+                    lr.SetPosition(1, transform.position);
+                }
+                if(t.phase == TouchPhase.Moved)
+                {
+                    pf = t.position;
+                    x = (pi.x - pf.x) * 0.03f;
+                    z = (pi.y - pf.y) * 0.03f;
+
+                    if(x > maxX)
+                    {
+                        x = maxX;
+                    }
+                    if (z > maxZ)
+                    {
+                        z = maxZ;
+                    }
+                    lr.SetPosition(1, new Vector3(
+                        transform.position.x + x,
+                        transform.position.y,
+                        transform.position.z + z));
+                }
+                if (t.phase == TouchPhase.Ended)
+                {
+                    GetComponent<Rigidbody>().AddForce(
+                        new Vector3(2 * x, 0, 2 * z),
+                        ForceMode.Impulse);
+                    lr.enabled = false;
+                    //if (GameManager.gm)
+                    //GameManager.gm.tacada();
+                }
+
+            }
         }
     }
 }
